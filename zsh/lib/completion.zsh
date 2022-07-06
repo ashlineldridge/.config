88a2d4b2713b75036db369c26180bfff9bdeb812
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2022-07-07 06:00:06 +1000

    zsh completion comments

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -7,6 +7,8 @@
 # e.g., `rustup completions zsh > /opt/homebrew/share/zsh/site-functions/_rustup`.
 autoload -U +X compinit; compinit
 
-# Configure fzf.
+# Configure fzf. These commands are taken from the changes fzf makes when you
+# run `$(brew --prefix)/opt/fzf/install` as per the installation instructions
+# here: https://github.com/junegunn/fzf#installation.
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
