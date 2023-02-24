a72964618dace6b9fba6972c7ecf22ff3adf57ab
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-02-24 13:00:25 +1000

    Mostly alacritty and tmux stuff

--- zsh/lib/env.zsh
+++ zsh/lib/env.zsh
@@ -4,6 +4,7 @@
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
 
 # XDG-based zsh home.
 export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
