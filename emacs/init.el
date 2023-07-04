3a59ba26d1a8ac31145494b3008f8e23672e8f70
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-07-04 21:49:56 +1000

    Minor detached.el changes

--- emacs/init.el
+++ emacs/init.el
@@ -2079,14 +2079,18 @@ buffer if necessary. If NAME is not specified, a buffer name will be generated."
         ([remap async-shell-command] . detached-shell-command)
         ([remap compile] . detached-compile)
         ([remap recompile] . detached-compile-recompile)
-        ;; Disable consult integration until the following issue is resolved:
+        ;; TODO: Disable consult integration until this issue is resolved:
         ;; https://lists.sr.ht/~niklaseklund/detached.el/%3CCAM-j%3Dqsnjw4%3D9kYbYGGR1oqC7BGxmZphN5Jq2gHdO3p8nQYdTw%40mail.gmail.com%3E.
         ;; ([remap detached-open-session] . detached-consult-session)
         ("C-c d d" . detached-open-session)
         ("C-c d l" . detached-list-sessions))
   :custom
   (detached-terminal-data-command system-type)
+  ;; TODO: Would prefer to use D-Bus or `alert' but haven't been able to
+  ;; to get either working properly on macOS.
   (detached-notification-function #'detached-state-transitionion-echo-message)
+  ;; Display git branch information in `detached-list-sessions'.
+  (detached-metadata-annotators-alist '((branch . detached--metadata-git-branch)))
   :init
   (detached-init))
 
