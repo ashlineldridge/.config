bbc526ace72d021ec4fb65b5c80ee73eeeca1357
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-11-16 12:51:23 +1100

    Upgrade Elpaca v0.6

--- emacs/elpaca-bootstrap.el
+++ emacs/elpaca-bootstrap.el
@@ -17,13 +17,13 @@
 
 ;; With the exception of including var/ in `elpaca-directory', the block below
 ;; has been copied verbatim from https://github.com/progfolio/elpaca#installer.
-(defvar elpaca-installer-version 0.5)
+(defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "var/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil
-                              :files (:defaults (:exclude "extensions"))
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
