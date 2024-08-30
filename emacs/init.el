467d009e4c9bd8bacc74dc9df73f57ec83ca99db
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2024-08-30 10:30:46 +1000

    [emacs] Unbind extra keybindings from diff-mode

--- emacs/init.el
+++ emacs/init.el
@@ -2355,7 +2355,8 @@
   :ensure nil
   :config
   ;; Unbind colliding keybindings.
-  (dolist (key '("M-0" "M-1" "M-2" "M-3" "M-4" "M-5" "M-6" "M-7" "M-8" "M-9" "M-k"))
+  (dolist (key '("M-0" "M-1" "M-2" "M-3" "M-4" "M-5" "M-6" "M-7" "M-8" "M-9"
+                 "M-k" "M-o"))
     (define-key diff-mode-map (kbd key) nil)))
 
 (use-package difftastic)
