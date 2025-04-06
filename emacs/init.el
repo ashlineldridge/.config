a8d1a4a708f9cac58b280b8e91060ba23834ef01
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2025-04-07 07:27:09 +1000

    [emacs] Fontaine presets: small, medium, large

--- emacs/init.el
+++ emacs/init.el
@@ -53,23 +53,32 @@
   (defconst my/variable-family "Iosevka Comfy Motion Duo")
   (defun my/fontaine-apply-preset ()
     "Apply the current (or default) Fontaine preset."
-    (fontaine-set-preset (or fontaine-current-preset 'regular)))
+    (fontaine-set-preset (or fontaine-current-preset 'small)))
   :bind
   ("C-c x f" . fontaine-set-preset)
   :hook
   (elpaca-after-init . my/fontaine-apply-preset)
   :custom
   (fontaine-presets
-   `((regular)
-     (large
+   `((small)
+     (medium
       :default-family ,my/fixed-family
       :default-height 140
-      :default-weight semibold
-      :fixed-pitch-weight semibold
-      :variable-pitch-weight semibold
+      :default-weight regular
+      :fixed-pitch-weight regular
+      :variable-pitch-weight regular
       :mode-line-active-height 140
       :mode-line-inactive-height 140
       :line-number-height 130)
+     (large
+      :default-family ,my/fixed-family
+      :default-height 150
+      :default-weight regular
+      :fixed-pitch-weight regular
+      :variable-pitch-weight regular
+      :mode-line-active-height 150
+      :mode-line-inactive-height 150
+      :line-number-height 140)
      (t
       :default-family ,my/fixed-family
       :default-height 130
