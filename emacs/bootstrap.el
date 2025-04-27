c04f1df32a61742d6556cd13c2a2f2e877f92b7d
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2025-04-28 09:52:27 +1000

    [emacs] Upgrade Elpaca to 0.11

--- emacs/bootstrap.el
+++ emacs/bootstrap.el
@@ -41,7 +41,7 @@
 
 ;; With the exception of including var/ in `elpaca-directory', the block below
 ;; has been copied verbatim from https://github.com/progfolio/elpaca#installer.
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "var/elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -76,7 +76,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
